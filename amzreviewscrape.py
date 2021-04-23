c4ac2fa45a4b1cdd78c6d969dac282d3a8ec1ad6
Abram Flansburg <abeflansburg@gmail.com>
2021-04-23 17:00:27 -0500

    Restore functionality, improve command line args, add pipfile for dependencies and update README

--- amzreviewscrape.py
+++ amzreviewscrape.py
@@ -6,27 +6,32 @@ import csv
 import io
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("-asins", dest="filename", required=True, help="Enter the path to your ASIN file.",
+argparser.add_argument("--asins", dest="filename", required=True, help="Enter the path to your ASIN file.",
+                       type=lambda x: is_valid_file(argparser, x))
+argparser.add_argument("--driverpath", dest="driver_path", required=False, help="Enter the path to your chromedriver file.",
                        type=lambda x: is_valid_file(argparser, x))
 args = argparser.parse_args()
 
 inputFile = args.filename
+driver_path = args.driver_path
 print(inputFile)
 
 # check for current os
-if os.name == 'posix':
-    # osx
-    driver_path = '/usr/local/bin/chromedriver'
-elif os.name == 'nt':
-    # win32
-    driver_path = 'C:\chromedriver\chromedriver'
-else:
-    print('Unknown operating system!!!')
-    exit()
+if driver_path is None:
+    if os.name == 'posix':
+        # osx
+        driver_path = '/usr/local/bin/chromedriver'
+    elif os.name == 'nt':
+        # win32
+        driver_path = 'C:\chromedriver\chromedriver'
+    else:
+        print('Unknown operating system!!!')
+        exit()
 
 data = read_reviews(driver_path, inputFile)
-
-field_names = ['asin', 'product_title', 'rating', 'review_title', 'variation', 'review_text', 'review-links']
+# *** Commenting out variations for now, as they may not work as expected ***
+# field_names = ['asin', 'product_title', 'rating', 'review_title', 'variation', 'review_text', 'review-links']
+field_names = ['asin', 'product_title', 'rating', 'review_title', 'review_text', 'review-links']
 
 expanded_reviews = []
 
@@ -36,16 +41,16 @@ for product_reviews in data:
     _data = product_reviews['data']
 
     for _d in _data:
-        expanded_reviews.append([_asin, _title, _d[0], _d[1], _d[2], _d[3], _d[4]])
+        expanded_reviews.append([_asin, _title, _d[0], _d[1], _d[2], _d[3]])
 
-with io.open('output.csv', 'w', encoding="utf-8", newline='') as dataFile:
+with io.open('./output/output.csv', 'w', encoding="utf-8", newline='') as dataFile:
     writer = csv.writer(dataFile, delimiter=',')
 
     writer.writerow(field_names)
     for e in expanded_reviews:
         writer.writerow(e)
 
-    print(f'Output written to "output.csv"')
+    print(f'Output written to "./output/output.csv"')
 
 
 
--- helpers.py
+++ helpers.py
@@ -6,6 +6,7 @@ import math
 import re
 import pprint
 import os.path
+import sys
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -38,8 +39,9 @@ def read_reviews(driver, file):
     products = []
 
     if len(asins) > 0:
+        print(f"\nWorking with {str(len(asins)) ASINS}:\n----------------------------")
         for asin in asins:
-            review_dict = {asin: {"ratings": [], "review-titles": [], "variations": [], "reviews": [], "review-links": [], }}
+            review_dict = {asin: {"ratings": [], "review-titles": [], "reviews": [], "review-links": [], }}
 
             # get reviews page count
             url = base_url + asin
@@ -47,11 +49,12 @@ def read_reviews(driver, file):
             source = browser.page_source
 
             soup = BS(source, 'html.parser')
-            # soup = soup.encode("utf-8")
 
-            total_reviews = soup.find('span', {'data-hook': 'total-review-count'})
-            total_reviews = int(total_reviews.text.replace(",",""))
-            page_count = int(math.ceil(total_reviews/10))
+            ratings_reviews_text = soup.find('div', {'data-hook': 'cr-filter-info-review-rating-count'}).span.contents
+            ratings_reviews_text = ratings_reviews_text[0].split('|')
+            ratings_count = int(re.sub(r'[^\d]', '', ratings_reviews_text[0]))
+            reviews_count = int(re.sub(r'[^\d]', '', ratings_reviews_text[1]))
+            page_count = int(math.ceil(reviews_count/10))
 
             # grab the title
             if soup.find('a', {'data-hook': 'product-link'}):
@@ -64,63 +67,58 @@ def read_reviews(driver, file):
                 product_title = 'No title found'
 
             if page_count > 0:
-                print(f'Page count: {str(page_count)}')
+                print(f'\nStarting parsing for ASIN {asin}\nPage count: {str(page_count)}:\n')
                 for i in range(page_count):
                     page = i + 1
                     page = str(page)
-                    print(f'Fetching page {page}')
+                    sys.stdout.write(f"\rParsing page {page} of {page_count}")
+                    sys.stdout.flush()
                     browser.get(url + f'/ref=cm_cr_getr_d_paging_btm_{page}?pageNumber={page}')
                     html = browser.page_source
-                    paged_soup = BS(html, 'html.parser')
-                    stars = paged_soup.find_all('div', {'data-hook': 'review'})
-                    stars = [s for s in stars if 'stars' in s.text]
+                    paged_soup = BS(html, 'html5lib')
+                    stars = paged_soup.find_all('i', {'data-hook': ['review-star-rating', 'cmps-review-star-rating']})
+                    stars = [s for s in stars if 'stars' in s.span.text]
                     for star in stars:
                         if 'stars' in star.text:
                             regex = "(\d.\d)"
                             p = re.compile(regex)
                             match = p.search(star.text)
                             review_dict[asin]['ratings'].append(match.group(0))
-                    review_titles = paged_soup.find_all('a',
-                          {'class': 'a-size-base a-link-normal review-title a-color-base a-text-bold'})
-                    review_titles = [r.text for r in review_titles]
-                    variations = paged_soup.find_all('span', {'class': 'a-color-secondary'})
-                    variations_2 = paged_soup.find_all('a', {'class': 'a-size-mini a-link-normal a-color-secondary'})
-                    if variations:
-                        variations = [v.text for v in variations if 'Color' in v.text or 'Size' in v.text]
-                    else:
-                        variations = []
-                    if variations_2:
-                        variations_2 = [v.text for v in variations_2 if 'Color' in v.text or 'Size' in v.text]
-                        for v in variations_2:
-                            variations.append(v)
-                    links = paged_soup.find_all('a',
-                          {'class': 'a-size-base a-link-normal review-title a-color-base a-text-bold'}, href=True)
-                    links = ["https://www.amazon.com%s" % l['href'] for l in links]
+                    review_titles_soup = paged_soup.findAll(re.compile(r'(i|a)'),{'data-hook': 'review-title'})
+
+                    review_titles = []
+                    links = []
+                    for review_title in review_titles_soup:
+                        if 'data-hook' not in review_title.previous_sibling.previous_sibling.attrs.keys() or review_title.previous_sibling.previous_sibling.attrs['data-hook'] != 'review-star-rating-view-point':
+                            review_titles.append(review_title)
+                            if review_title.name == 'a':
+                                links.append(f"https://www.amazon.com{review_title.attrs['href']}")
+                            else:
+                                links.append('')
+                    # links = [f"https://www.amazon.com{l['href']}" for l in links]
                     for ll in links:
                         review_dict[asin]['review-links'].append(ll)
                     for rt in review_titles:
-                        review_dict[asin]['review-titles'].append(rt)
+                        sp = rt.findChild('span')
+                        if len(rt.findChild('span')):
+                            review_dict[asin]['review-titles'].append(re.sub(r'\n', '', rt.span.text))
+                        else:
+                            review_dict[asin]['review-titles'].append(re.sub(r'\n', '', rt))
                     review_text = paged_soup.find_all('span', {'data-hook': 'review-body'})
-                    review_text = [rev.text.replace('\U0001f44d', '').replace('\U0001f4a9', '') for rev in review_text]
+
+                    review_text = [rev.span.text.replace('\U0001f44d', '').replace('\U0001f4a9', '') if rev.span else '' for rev in review_text]
                     for review in review_text:
-                        review_dict[asin]['reviews'].append(review)
-                    if len(variations) != 0:
-                        for v in variations:
-                            review_dict[asin]['variations'].append(v)
-                    else:
-                        review_dict[asin]['variations'] = []
+                        review_dict[asin]['reviews'].append(re.sub(r'\n', '', review))
             data_tuples = []
-            for rr in range(len(review_dict[asin]['reviews'])):
+            for rr in range(len(review_dict[asin]['reviews'])-1):
                 try:
                     data_tuples.append((review_dict[asin]['ratings'][rr], review_dict[asin]['review-titles'][rr],
-                                        review_dict[asin]['variations'][rr], review_dict[asin]['reviews'][rr], 
+                                        review_dict[asin]['reviews'][rr],
                                         review_dict[asin]['review-links'][rr]))
                 except IndexError:
-                    data_tuples.append((review_dict[asin]['ratings'][rr], review_dict[asin]['review-titles'][rr],
-                                        'N/A', review_dict[asin]['reviews'][rr], 
-                                        review_dict[asin]['review-links'][rr]))
+                    print('There was an index out of range error - this typically means the review HTML has changed somehow.')
             products.append({"asin": asin, "title": product_title, "data": data_tuples})
-
+            print(f"\n\nReviews for ASIN {asin} parsed & written to CSV\n----------------------------")
         browser.close()
         # should return an object with all info here (or write out to csv)
         return products
